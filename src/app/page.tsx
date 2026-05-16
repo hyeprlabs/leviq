7f5e1d528ef494213b4ce03abe097d445044c470
Oskar-Maria Seeberger <110058070+hyepr@users.noreply.github.com>
2026-05-16 16:51:29 +0000

    Prettier Write!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import { HeroSection } from "@/components/hero";
 import { LogosSection } from "@/components/logos-section";
 
 export default function Page() {
-	return (
-		<div className="relative flex min-h-screen flex-col overflow-hidden px-4 supports-[overflow:clip]:overflow-clip">
-			<Header />
-			<main
-				className={cn(
-					"relative mx-auto max-w-4xl grow",
-					// X Borders
-					"before:absolute before:-inset-y-14 before:-left-px before:w-px before:bg-border",
-					"after:absolute after:-inset-y-14 after:-right-px after:w-px after:bg-border"
-				)}
-			>
-				<HeroSection />
-				<LogosSection />
-			</main>
-		</div>
-	);
+  return (
+    <div className="relative flex min-h-screen flex-col overflow-hidden px-4 supports-[overflow:clip]:overflow-clip">
+      <Header />
+      <main
+        className={cn(
+          "relative mx-auto max-w-4xl grow",
+          // X Borders
+          "before:absolute before:-inset-y-14 before:-left-px before:w-px before:bg-border",
+          "after:absolute after:-inset-y-14 after:-right-px after:w-px after:bg-border",
+        )}
+      >
+        <HeroSection />
+        <LogosSection />
+      </main>
+    </div>
+  );
 }
