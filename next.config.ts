5025ec198c4d3e2c202acfe72902e76e5aa96d7d
Oskar-Maria Seeberger <110058070+hyepr@users.noreply.github.com>
2026-06-16 18:47:23 +0000

    Fixed Origin Errors and generated importMap!

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["*.app.github.dev"],
   /* config options here */
 };
 
