507b6c6538be48d34b45e3a2cda257f149cd4cd3
Oskar-Maria Seeberger <110058070+hyepr@users.noreply.github.com>
2026-06-16 21:00:46 +0000

    Run Migration!

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260616_205835 from './20260616_205835';
+
+export const migrations = [
+  {
+    up: migration_20260616_205835.up,
+    down: migration_20260616_205835.down,
+    name: '20260616_205835'
+  },
+];
